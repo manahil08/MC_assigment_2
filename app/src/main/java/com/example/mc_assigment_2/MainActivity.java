8f4c702124adb2ce8b99480b0188857673b6f5b6
manahil <bitf18a507@pucit.edu.pk>
2021-11-09 20:20:35 +0500

    3rd commit

--- app/src/main/java/com/example/mc_assigment_2/MainActivity.java
+++ app/src/main/java/com/example/mc_assigment_2/MainActivity.java
@@ -3,17 +3,26 @@ package com.example.mc_assigment_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
+    Button button_1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button=(Button)findViewById(R.id.button);
+        button_1=(Button)findViewById(R.id.button2);
+        button_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoUrl("https://github.com/manahil08/MC_assigment_2.git");
+            }
+        });
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,5 +34,10 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(my_intent);
             }
         });
+    }
+
+    private void gotoUrl(String s) {
+        Uri uri= Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW,uri));
     }
 }
